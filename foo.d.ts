d1a68be5e3e8ba445cc09989a6aa458bf8c79c28
Aleksey Kliger <aleksey@lambdageek.org>
2020-10-28 10:51:34 -0400

    initial Module.wasmMemory explicitly

--- foo.d.ts
+++ foo.d.ts
@@ -1,12 +1,28 @@
 
 export = Module;
 
-declare function Module(): Promise<Module.FooItf>;
+declare function Module<Inp extends Module.InputModule>(inputModule?: Inp): Promise<Inp&Module.FooItf&Module.Module>;
 
 declare namespace Module {
+
+    /// Properties that can be set to affect the emscripten wasm module
+    export interface InputModule {
+        INITIAL_MEMORY?: number;
+        wasmMemory?: WebAssembly.Memory;
+    }
+
+    export interface Module {
+        HEAP8: Int8Array;
+        HEAP16: Int16Array;
+        HEAP32: Int32Array;
+        HEAPU8: Uint8Array;
+        HEAPU16: Uint16Array;
+        HEAPU32: Uint32Array;
+    }
 
     export interface FooItf {
         _initialize (): void;
         _foo (): number;
     }
+
 }
--- index.ts
+++ index.ts
@@ -13,18 +13,34 @@ class C {
 
     Foo = async () => {
         this.Put ('abcd');
-        let foo  = await Foo ();
+        const WASM_PAGE_SIZE = 65536;
+        const TOTAL_STACK = 5242880; // this seems hardcoded in Emscripten
+        const initialMem = 10 + (TOTAL_STACK / WASM_PAGE_SIZE); // this should match c/Makefile
+        let mem = new WebAssembly.Memory ({initial: initialMem, maximum: 2*initialMem});
+        let moduleIn = {"wasmMemory": mem, "INITIAL_MEMORY": initialMem * WASM_PAGE_SIZE};
+        let foo = await Foo (moduleIn);
         foo._initialize();
         for (let i = 0; i < 10; ++i) {
             await C.sleep (50);
             let n = foo._foo ();
             this.Put (document.createElement('br'), 'hijk ' + n);
         }
+        //this._dumpObject(foo, "foo");
+        this.Put (document.createElement("hr"));
+        this.Put (`memory has ${mem.buffer.byteLength} bytes`);
     }
 
     Put = (...content: (string | Node)[]) => {
         this.#boxy.append(...content);
-   }
+    }
+
+    _dumpObject = (foo: object, name?: string) => {
+        name = (name === undefined) ? "foo" : name;
+        this.Put (document.createElement("hr"));
+        Object.keys(foo).forEach((k, _idx, _arr) => {
+            this.Put (`${name}.${k}`, document.createElement("br"));
+        })
+    }
 }
 
 document.addEventListener('DOMContentLoaded', (_event) => {
